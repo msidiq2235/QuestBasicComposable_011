3820853e52dda9f6fcaa270cfc775a71c2cb6b35
msidiq2235 <muhsidiq2005@gmail.com>
2025-10-17 15:24:09 +0700

    menambah fungsi tataletakcolumn

--- app/src/main/java/com/example/coba/Tataletak.kt
+++ app/src/main/java/com/example/coba/Tataletak.kt
@@ -1,2 +1,18 @@
 package com.example.coba
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun TataLetakColumn(modifier: Modifier) {
+    Column(modifier = modifier.padding(top = 20.dp, start = 20.dp, end = 20)) {
+        Text(text = "Komponen1")
+        Text(text = "Komponen2")
+        Text(text = "Komponen3")
+        Text(text = "Komponen4")
+    }
+}
